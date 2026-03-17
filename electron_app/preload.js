a321364348e96ad41592bc07352a5846aeb7dbe6
M1ghty Yu <egorrajder@gmail.com>
2026-03-17 03:39:16 +0300

    Remove source code, keep README and screenshots only

--- electron_app/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-  getBackendUrl: () => ipcRenderer.invoke('get-backend-url'),
-});
